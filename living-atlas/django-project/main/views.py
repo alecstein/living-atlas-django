6a40cad44ef216eb5215ff91f822b8a9e210d641
Alec Stein <alecstein@g.ucla.edu>
2021-06-06 19:21:27 -0500

    added multiple lemma search

--- living-atlas/django-project/main/views.py
+++ living-atlas/django-project/main/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import FrolexEntry
-from .forms import VariantForm
-from django.forms import formset_factory
-VariantFormSet = formset_factory(VariantForm)
+from django.db.models import Q
+Q(question__startswith='What')
+
+# from .forms import VariantForm
+# from django.forms import formset_factory
+# VariantFormSet = formset_factory(VariantForm)
 
 # Create your views here.
 
 def search_view(request):
     query = request.GET.get('q')
+    if query is not None:
+        querylist = [query.strip() for query in query.split(',')]
+        for lemma in querylist:
+            print(lemma)
+    else:
+        querylist = None
     show_search = False
 
     if query:
         show_search = True
-        results = FrolexEntry.objects.filter(lemma=query)
+        results = []
+        for lemma in querylist:
+            results.extend(FrolexEntry.objects.filter(lemma = lemma))
     else:
         results = None
     context = {
         'results':results, 
         'show_search':show_search,
         'query':query,
+        'querylist':querylist,
         }
     return render(request, 'search.html', context)
 
