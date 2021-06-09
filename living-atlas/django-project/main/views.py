7bf9c0382c0ce90e5dc184f5ea1c6f45b3cc9cf4
Alec Stein <alecstein@g.ucla.edu>
2021-06-09 15:05:46 -0500

    added session form handling
    Signed-off-by: Alec Stein <alecstein@g.ucla.edu>

--- living-atlas/django-project/main/views.py
+++ living-atlas/django-project/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import FrolexEntry
+from django.shortcuts import redirect
 from .models import Form
 import json
 from time import time
@@ -11,6 +11,7 @@ from time import time
 # %%
 def search1_view(request):
     """
+    The first search that the user makes is called 'search1'.
     The app revolves around this view, which is also the homepage.
     show_search: bool. whether or not to show the search results
     querylist: list of strings that are queried
@@ -23,84 +24,57 @@ def search1_view(request):
     """
 
     context = {}
+    queryset = Form.objects.all()
+    request.session.flush() # start a new session
 
     if request.method == 'GET':
-        # Check to see if a non-regex query was submitted
+
         if request.GET.get('q'):
+            # Normal query type
             query = request.GET.get('q')
             # Only accept lemmas which are not the empty string ''
             lemmas = set([lemma for lemma in query.strip().splitlines() if lemma.strip()])
             if len(lemmas) == 0:
                 # If no valid lemmas are found, return invalid search page
                 context['valid_search'] = False
-                return render(request, 'search1.html', context)
+            else:
+                # Get all matching forms and lemmas
+                context['valid_search'] = True
+                forms = queryset.filter(lemma__in = lemmas)
 
-            # Get all matching forms and lemmas
-            context['valid_search'] = True
-            forms = Form.objects.filter(lemma__in = lemmas)
-
-            # Build a dictionary of the forms and lemmas
-            # Lemmas are keys, a list of forms is the value
-            form_dict = {}
-            for form in forms:
-                if form.form != form.lemma:
-                    # Only include forms
-                    try:
-                        form_dict[form.lemma].append(form.form)
-                    except KeyError:
-                        form_dict[form.lemma] = [form.form]
-
-            context['result_dict'] = form_dict
-            return render(request, 'search1.html', context)
-
-        if request.GET.get('r'):
-            # Check to see if a regex query was submitted
+        elif request.GET.get('r'):
+            # Regex query type
             query = request.GET.get('r')
             if query == '':
                 context['valid_search'] == False
-                return render(request, 'search1.html', context)
-
-            # Get all matching forms and lemmas
-            context['valid_search'] = True
-            forms = Form.objects.filter(lemma__regex =fr"{query}")[:2000]
-
-            s = time()
-            form_dict = {}
-            for form in forms:
-                if form.form != form.lemma:
-                    # Only include forms
-                    try:
-                        form_dict[form.lemma].append(form.form)
-                    except KeyError:
-                        form_dict[form.lemma] = [form.form]
-            print(time()-s)
-
-            context['result_dict'] = form_dict
-            # print(result_dict)
+            else:
+                # Get all matching forms and lemmas
+                context['valid_search'] = True
+                forms = queryset.filter(lemma__regex =fr"{query}")[:2000]
+        else:
+            # Should be unreachable
             return render(request, 'search1.html', context)
 
+        form_dict = {}
+        for form in forms:
+            if form.form != form.lemma:
+                # Only include forms
+                try:
+                    form_dict[form.lemma].append(form.form)
+                except KeyError:
+                    form_dict[form.lemma] = [form.form]
 
-    if request.method == 'POST':
-        # Form was submitted
-        keys = [key for key in request.POST if key.startswith('checkbox')]
-        selections = []
-        for key in keys:
-            selections.extend(request.POST.getlist(key))
-
-        if len(selections) == 0:
-            context['valid_submission'] = False
-            return render(request, 'search1.html', context)
-
-        context['valid_submission'] = True
-        context['selections'] = selections
-
-        return render(request, 'search2.html', context)
+        context['result_dict'] = form_dict
+        request.session['search1_successful'] = True  
 
     return render(request, 'search1.html', context)
 
 def search2_view(request):
     """
-    The app revolves around this view, which is also the homepage.
+    The second search the user sees is called 'search2'.
+    This search combines the form data from search1 with the 
+    form data from search2 and outputs the results.
+
     show_search: bool. whether or not to show the search results
     querylist: list of strings that are queried
     query: whatever someone types in the searchbox
@@ -112,65 +86,19 @@ def search2_view(request):
     """
 
     context = {}
+    queryset = Form.objects.all()
+    
+    # If we haven't succeeded with search1, return to search1
+    if not request.session.get('search1_successful', False):
+        print('redirected')
+        return redirect('/1/')
 
-    if request.method == 'GET':
-        # Check to see if a non-regex query was submitted
-        if request.GET.get('q'):
-            query = request.GET.get('q')
-            # Only accept lemmas which are not the empty string ''
-            lemmas = set([lemma for lemma in query.strip().splitlines() if lemma.strip()])
-            if len(lemmas) == 0:
-                # If no valid lemmas are found, return invalid search page
-                context['valid_search'] = False
-                return render(request, 'search2.html', context)
-
-            # Get all matching forms and lemmas
-            context['valid_search'] = True
-            forms = Form.objects.filter(lemma__in = lemmas)
-
-            # Build a dictionary of the forms and lemmas
-            # Lemmas are keys, a list of forms is the value
-            form_dict = {}
-            for form in forms:
-                if form.form != form.lemma:
-                    # Only include forms
-                    try:
-                        form_dict[form.lemma].append(form.form)
-                    except KeyError:
-                        form_dict[form.lemma] = [form.form]
-
-            context['result_dict'] = form_dict
-            return render(request, 'search2.html', context)
-
-        if request.GET.get('r'):
-            # Check to see if a regex query was submitted
-            query = request.GET.get('r')
-            if query == '':
-                context['valid_search'] == False
-                return render(request, 'search2.html', context)
-
-            # Get all matching forms and lemmas
-            context['valid_search'] = True
-            forms = Form.objects.filter(lemma__regex =fr"{query}")[:2000]
-
-            s = time()
-            form_dict = {}
-            for form in forms:
-                if form.form != form.lemma:
-                    # Only include forms
-                    try:
-                        form_dict[form.lemma].append(form.form)
-                    except KeyError:
-                        form_dict[form.lemma] = [form.form]
-            print(time()-s)
-
-            context['result_dict'] = form_dict
-            # print(result_dict)
-            return render(request, 'search2.html', context)
-
+    # If we have succeeded with both search1 and search2, complete
+    # if request.session.get('search2_successful', False):
+    #     request.session.flush()
+    #     return HttpResponse('data submitted')
 
     if request.method == 'POST':
-        # Form was submitted
         keys = [key for key in request.POST if key.startswith('checkbox')]
         selections = []
         for key in keys:
@@ -178,12 +106,66 @@ def search2_view(request):
 
         if len(selections) == 0:
             context['valid_submission'] = False
+            print('no selections')
             return render(request, 'search2.html', context)
 
-        context['valid_submission'] = True
-        context['selections'] = selections
+        # If we have not completed search2, update the 
+        # session data for search1
+        if not request.session.get('search2_successful', False):
+            request.session['search1_selections'] = selections # save the selections
+            context['search1_selections'] = selections
 
+        else:
+            request.session['search2_selections'] = selections
+            context['search2_selections'] = selections
+            print(request.session['search2_selections'])
+            print(request.session['search1_selections'])
+            return HttpResponse('data submitted')
+        
+        print("rendered post")
         return render(request, 'search2.html', context)
+
+    if request.method == 'GET':
+
+        if request.GET.get('q'):
+            # Normal query type
+            query = request.GET.get('q')
+            # Only accept lemmas which are not the empty string ''
+            lemmas = set([lemma for lemma in query.strip().splitlines() if lemma.strip()])
+            if len(lemmas) == 0:
+                # If no valid lemmas are found, return invalid search page
+                context['valid_search'] = False
+            else:
+                # Get all matching forms and lemmas
+                context['valid_search'] = True
+                forms = queryset.filter(lemma__in = lemmas)
+        elif request.GET.get('r'):
+            # Regex query type
+            query = request.GET.get('r')
+            if query == '':
+                context['valid_search'] == False
+            else:
+                # Get all matching forms and lemmas
+                context['valid_search'] = True
+                forms = queryset.filter(lemma__regex =fr"{query}")[:2000]
+        else:
+            # Should be unreachable
+            print("reached unreachable")
+            return render(request, 'search1.html', context)
+
+        form_dict = {}
+        for form in forms:
+            if form.form != form.lemma:
+                # Only include forms
+                try:
+                    form_dict[form.lemma].append(form.form)
+                except KeyError:
+                    form_dict[form.lemma] = [form.form]        
+
+        request.session['search2_successful'] = True  
+        context['result_dict'] = form_dict 
+        context['search1_selections'] = request.session.get('search1_selections')
+        print(request.session.get('search1_selections'))
 
     return render(request, 'search2.html', context)
  
