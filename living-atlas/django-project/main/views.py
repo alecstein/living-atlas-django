1907315db2f306491ccf0e3b5c3648b046a1fdfa
Alec Stein <alecstein@g.ucla.edu>
2021-06-08 19:07:18 -0500

    removed unnecessary files

--- living-atlas/django-project/main/views.py
+++ living-atlas/django-project/main/views.py
@@ -64,64 +64,11 @@ def search_view(request):
 
         context['valid_submission'] = True
         context['selections'] = selections
+
         return render(request, 'search.html', context)
-
-    # if request.GET.('q'):
-    #     # Check to see if a search was submitted
-
-    #     lemmas = set([])
-
-
-
-    # query = request.GET.get('q')
-    # if query:
-    #     # Don't do any searching the user types in newlines and spaces
-    #     querylist = set([s for s in query.strip().splitlines(True) if s.strip()])
-    #     if len(querylist) == 0:
-    #         query = None
-
-    # # This is the logic used for getting the selected checkboxes
-    # # after the search is done.
-    # # We loop over all the separate checkbox names and make them into a list
-    # input_keys = [key for key in request.POST if key.startswith("ckbox")]
-    # selected_checkboxes = []
-    # for key in input_keys:
-    #     selected_checkboxes.extend(request.POST.getlist(key))
-
-    # if query:
-    #     search_results = True
-    #     results = []
-    #     for lemma in querylist:
-    #         results.extend(FrolexEntry.objects.filter(lemma = lemma))
-
-    #     found_lemmas = set([result.lemma for result in results if result.lemma == result.form])
-    #     # Note: NOT a django form -- linguistic "form"
-    #     found_forms = set([result for result in results if result.lemma != result.form])
-
-    #     previous_query = ''
-    #     for queryitem in querylist:
-    #         previous_query += queryitem + '\n'
-    # else:
-    #     results = None
-    #     search_results = False
-    #     querylist = None
-    #     found_lemmas = None
-    #     found_forms = None
-    #     previous_query = None
-
-    # context = {
-    #     'results':results, 
-    #     'search_results':search_results,
-    #     'query':query,
-    #     'querylist':querylist,
-    #     'selected_checkboxes':selected_checkboxes,
-    #     'found_lemmas':found_lemmas,
-    #     'found_forms':found_forms,
-    #     'previous_query':previous_query,
-    #     }
-
+        
     return render(request, 'search.html', context)
-
+ 
 def about_view(request):
     return render(request, 'about.html', {})
 
