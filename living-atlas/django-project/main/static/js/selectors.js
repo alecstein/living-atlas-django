dbcf04ec53a3d73cf168949e370f826091818022
Alec Stein <alecstein@g.ucla.edu>
2021-06-08 19:01:58 -0500

    refactored entire application
    Signed-off-by: Alec Stein <alecstein@g.ucla.edu>

---
+++ living-atlas/django-project/main/static/js/selectors.js
@@ -0,0 +1,36 @@
+var activeLemma = null;
+
+function selectForms(item) {
+  // When a lemma is clicked, it reveals the forms associated with it
+  // and highlights that lemma
+
+  if (activeLemma != null) {
+    const lemmaToDehighlight = document.getElementById(activeLemma);
+    lemmaToDehighlight.setAttribute("style", "background-color:white");
+    const formsToDisable = document.getElementsByName(activeLemma.concat("-child"));
+    for (var i = formsToDisable.length - 1; i >= 0; i--) {
+      formsToDisable[i].setAttribute("style", "display:none");
+    }
+  }
+
+  const lemma = item.id;
+  const lemmaToHighlight = document.getElementById(lemma);
+  lemmaToHighlight.setAttribute("style", "background-color:#ffe600");
+  const formsToEnable = document.getElementsByName(lemma.concat("-child"));
+  for (var i = formsToEnable.length - 1; i >= 0; i--) {
+    // forms[i].style.display = "visible";
+    formsToEnable[i].setAttribute("style", "display:visible");
+  }
+  activeLemma = lemma;
+}
+
+function lemmaToggleAll(item) {
+  // Toggles all the forms associated with a lemma when 
+  // that lemma's checkbox is checked
+
+  const lemma = item.id;
+  formsToToggle = document.getElementsByName(lemma.concat("-child"));
+  for (var i = formsToToggle.length - 1; i >= 0; i--) {
+    formsToToggle[i].checked = item.checked;
+  }
+}
--- living-atlas/django-project/main/views.py
+++ living-atlas/django-project/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import FrolexEntry
+import json
 
 # Create your views here.
 
+# %%
+
+# %%
 def search_view(request):
     """
     The app revolves around this view, which is also the homepage.
@@ -17,47 +21,104 @@ def search_view(request):
     the lemma(s) selected by the user
     """
 
-    query = request.GET.get('q')
+    context = {}
 
-    # This is the logic used for getting the selected checkboxes
-    # We loop over all the separate checkbox names and make them into a list
-    input_keys = [key for key in request.POST if key.startswith("ckbox")]
-    selected_checkboxes = []
-    for key in input_keys:
-        selected_checkboxes.extend(request.POST.getlist(key))
+    if request.method == 'GET':
+        # Check to see if a query was submitted
+        if request.GET.get('q'):
+            query = request.GET.get('q')
+            # Only accept lemmas which are not the empty string ''
+            lemmas = set([lemma for lemma in query.strip().splitlines() if lemma.strip()])
+            if len(lemmas) == 0:
+                # If no valid lemmas are found, return invalid search page
+                context['valid_search'] = False
+                return render(request, 'search.html', context)
 
-    if query:
-        querylist = [query.strip('\n').strip() for query in query.strip().split('\n')]
-        search_results = True
-        results = []
-        for lemma in querylist:
-            results.extend(FrolexEntry.objects.filter(lemma = lemma))
+            # Get all matching forms and lemmas
+            context['valid_search'] = True
+            results = FrolexEntry.objects.filter(lemma__in = lemmas)
 
-        found_lemmas = set([result.lemma for result in results if result.lemma == result.form])
-        # Note: NOT a django form -- linguistic "form"
-        found_forms = set([result for result in results if result.lemma != result.form])
+            # Build a dictionary of the forms and lemmas
+            # Lemmas are keys, a list of forms is the value
+            result_dict = {}
+            for result in results:
+                if result.form != result.lemma:
+                    # Only include forms
+                    try:
+                        result_dict[result.lemma].append(result.form)
+                    except KeyError:
+                        result_dict[result.lemma] = [result.form]
+            context['result_dict'] = result_dict
+            return render(request, 'search.html', context)
 
-        previous_query = ''
-        for queryitem in querylist:
-            previous_query += queryitem + '\n'
-    else:
-        results = None
-        search_results = False
-        querylist = None
-        found_lemmas = None
-        found_forms = None
-        previous_query = None
+    if request.method == 'POST':
+        # Form was submitted
+        keys = [key for key in request.POST if key.startswith('checkbox')]
+        selections = []
+        for key in keys:
+            selections.extend(request.POST.getlist(key))
 
-    context = {
-        'results':results, 
-        'search_results':search_results,
-        'query':query,
-        'querylist':querylist,
-        'selected_checkboxes':selected_checkboxes,
-        'found_lemmas':found_lemmas,
-        'found_forms':found_forms,
-        'previous_query':previous_query,
-        }
+        if len(selections) == 0:
+            context['valid_submission'] = False
+            return render(request, 'search.html', context)
+
+        context['valid_submission'] = True
+        context['selections'] = selections
+        return render(request, 'search.html', context)
+
+    # if request.GET.('q'):
+    #     # Check to see if a search was submitted
+
+    #     lemmas = set([])
+
+
+
+    # query = request.GET.get('q')
+    # if query:
+    #     # Don't do any searching the user types in newlines and spaces
+    #     querylist = set([s for s in query.strip().splitlines(True) if s.strip()])
+    #     if len(querylist) == 0:
+    #         query = None
+
+    # # This is the logic used for getting the selected checkboxes
+    # # after the search is done.
+    # # We loop over all the separate checkbox names and make them into a list
+    # input_keys = [key for key in request.POST if key.startswith("ckbox")]
+    # selected_checkboxes = []
+    # for key in input_keys:
+    #     selected_checkboxes.extend(request.POST.getlist(key))
+
+    # if query:
+    #     search_results = True
+    #     results = []
+    #     for lemma in querylist:
+    #         results.extend(FrolexEntry.objects.filter(lemma = lemma))
+
+    #     found_lemmas = set([result.lemma for result in results if result.lemma == result.form])
+    #     # Note: NOT a django form -- linguistic "form"
+    #     found_forms = set([result for result in results if result.lemma != result.form])
+
+    #     previous_query = ''
+    #     for queryitem in querylist:
+    #         previous_query += queryitem + '\n'
+    # else:
+    #     results = None
+    #     search_results = False
+    #     querylist = None
+    #     found_lemmas = None
+    #     found_forms = None
+    #     previous_query = None
+
+    # context = {
+    #     'results':results, 
+    #     'search_results':search_results,
+    #     'query':query,
+    #     'querylist':querylist,
+    #     'selected_checkboxes':selected_checkboxes,
+    #     'found_lemmas':found_lemmas,
+    #     'found_forms':found_forms,
+    #     'previous_query':previous_query,
+    #     }
 
     return render(request, 'search.html', context)
 
