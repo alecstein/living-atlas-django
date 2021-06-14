a28bb959c2383484ac29ec6da96ccea4fba8615c
Alec Stein <alecstein@g.ucla.edu>
2021-06-14 15:42:47 -0500

    added comment

--- living-atlas/import-from-csv.py
+++ living-atlas/import-from-csv.py
@@ -2,6 +2,9 @@ import csv
 from tqdm import tqdm
 from main.models import Form
 
+# Run with
+# ./manage.py shell < import-from-csv.py
+
 with open('../medieval-french-lang-toolkit/frolex-forms.csv') as f:
     reader = csv.reader(f)
     print(next(reader))
