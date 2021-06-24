2a92f8e72bc640878290615fa450f75706d6948f
Alec Stein <alecstein@g.ucla.edu>
2021-06-23 23:37:24 -0500

    removed secret key

--- living-atlas/living-atlas/settings.py
+++ living-atlas/living-atlas/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xcnnx($&lx5y+zq=%gtud-f*p&g+wv3i3jtitbgzr_4__@5)tf'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
