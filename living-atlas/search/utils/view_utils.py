58aa7a87db971649789297286142879f8ab22528
Alec Stein <alecstein@g.ucla.edu>
2021-06-25 11:07:48 -0500

    fixed bug with selectors and cleaned up utils

--- living-atlas/search/utils/view_utils.py
+++ living-atlas/search/utils/view_utils.py
@@ -12,13 +12,10 @@ def render_to_carto_response(request):
     json_data = {}
 
     for key in request.POST:
-        if "@" not in key: 
+        try:
+            lemma_name, latin, homonym_id, group, form = key.split("@")
+        except ValueError:
             continue
-        value = request.POST[key]
-        if "@" not in value:
-            continue
-        group, form = value.split("@")
-        
         json_data.setdefault(group, []).append(form)
 
     return JsonResponse(json_data, status=200)
@@ -28,32 +25,38 @@ from openpyxl import Workbook
 def render_to_excel_response(request):
 
     response = HttpResponse()
-    response['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    response['Content-Disposition'] = f'attachment; filename=living-atlas.xlsx'
+    response['Content-Type'] = 'application\
+                               /vnd.openxmlformats-officedocument.\
+                               spreadsheetml.sheet'
+    response['Content-Disposition'] = f'attachment; \
+                                    filename=living-atlas.xlsx'
 
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.title = "living-atlas-export"
 
-    columns = {'form'  : 1, 
-               'lemma' : 2, 
-               'latin' : 3, 
-               'group' : 4,}
+    columns = {'form'       : 1,
+               'lemma'      : 2,
+               'latin'      : 3,
+               'homonym_id' : 4,
+               'group'      : 5,}
 
-    # Create the column names
     for col_name, col_idx in columns.items():
         cell = worksheet.cell(row = 1, column = col_idx)
         cell.value = col_name
 
-    # Insert the row values
     row_idx = 2
     for key in request.POST:
-        data = key.split("@")
-        if data != 4:
+        try:
+            lemma_name, latin, homonym_id, group, form = key.split("@")
+        except ValueError:
             continue
 
-        lemma, latin, group, form = data
-        row_values = {'form':form, 'lemma':lemma, 'latin':latin, 'group':group}
+        row_values = {'form'      : form,
+                      'lemma'     : lemma, 
+                      'latin'     : latin,
+                      'homonym_id': homonym_id,
+                      'group'     : group,}
 
         for col_name, row_value in row_values.items():
             cell = worksheet.cell(row = row_idx, column = columns[col_name])
