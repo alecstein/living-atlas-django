dbabe6bfeedb0f77dcf5ff647cefc338a7940ed5
Alec Stein <alecstein@g.ucla.edu>
2021-06-25 00:47:28 -0500

    cleaned up view

--- living-atlas/search/views.py
+++ living-atlas/search/views.py
@@ -14,12 +14,10 @@ regex_substitutions = {
 def ajax_view(request):
     """
     Server API endpoint for fetching data
-    N: limit of number of queries to fetch
     Note that 'form' is a linguistic object -- it does NOT have
     anything to do with a Django Form.
     """
 
-    timeout = 4 # seconds
     context = {}
     status = 200
 
@@ -29,7 +27,7 @@ def ajax_view(request):
         query_type = request.GET.get('type')
         group = request.GET.get('group')
         lang = request.GET.get('lang')
-        form_filter = request.GET.get('form_filter', '')
+        form_filter = request.GET.get('form_filter')
 
         lemma_queryset = Lemma.objects.prefetch_related('form_set')
 
@@ -37,10 +35,11 @@ def ajax_view(request):
             query_items_set = set(raw_query.split(' '))
             if lang == 'lemma':
                 lemma_queryset = lemma_queryset.filter(name__in = query_items_set)
+                found_items_set = set(lemma_queryset.values_list('name', flat = True))
             elif lang == 'latin':
                 lemma_queryset = lemma_queryset.filter(latin__in = query_items_set)
+                found_items_set = set(lemma_queryset.values_list('latin', flat = True))
 
-            found_items_set = set(lemma_queryset.values_list(lang, flat = True))
             not_found_items_set = query_items_set.difference(found_items_set)
             context['not_found_items_set'] = not_found_items_set
 
@@ -53,32 +52,25 @@ def ajax_view(request):
             elif lang == 'latin':
                 lemma_queryset = lemma_queryset.filter(latin__regex = raw_query)
 
-        
+
         results_dict = {}
+        lemma_queryset = lemma_queryset[:300]
 
-        start_time = time()
-        for lemma in tqdm(lemma_queryset):
+        for lemma in lemma_queryset:
 
-            # if time() - start_time > timeout:
-                # status = 408
-                # break
-
-            lemma_name = lemma.name
-            latin      = lemma.latin
-            homonym_id = lemma.homonym_id
-
+            lemma_name    = lemma.name
+            latin         = lemma.latin
+            homonym_id    = lemma.homonym_id
             form_queryset = lemma.form_set
+
             if form_filter:
                 form_queryset = form_queryset.filter(name__regex = form_filter)
+
             form_list = form_queryset.values_list("name", flat=True)
 
             results_dict[lemma_name] = {'form_list':form_list,
                                         'latin':latin,
                                         'homonym_id':homonym_id}
-
-        end_time = time()
-        print('old way')
-        print(end_time - start_time)
 
         if not results_dict:
             raise Http404("No result matches the given query.")
